40fab79effab88162565524dcf68ef3355c92d64
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-11-21 03:53:38 +0700

    add join string list node.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
 from .create_n_token_node import CreateNTokenStringNode
 from .join_string_node import JoinStringsNode
-from .split_string import SplitStringNode
+from .split_string_node import SplitStringNode
+from .join_string_list_node import JoinStringListNode
 
 NODE_CLASS_MAPPINGS = {
     "Text Utils - String Subparts": CreateNTokenStringNode,
     "Text Utils - Join Strings": JoinStringsNode,
     "Text Utils - Split String to List": SplitStringNode,
+    "Text Utils - Join String List": JoinStringListNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Text Utils - String Subparts": "String Subparts",
     "Text Utils - Join Strings": "Join Strings",
     "Text Utils - Split String to list": "Split String to List",
+    "Text Utils - Join String List": "Join String List to one string",
 }
--- join_string_list_node.py
+++ join_string_list_node.py
@@ -1,18 +1,17 @@
-class SplitStringNode:
+class JoinStringListNode:
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "text": ("STRING", {"default": ""}),
+                "texts": ("STRING", {"forceInput": True}),
                 "separator": ("STRING", {"default": "/"}),
             },
         }
-
+    INPUT_IS_LIST = True
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("TEXT",)
-    OUTPUT_IS_LIST = (True, )
-    FUNCTION = "perform_split_string"
+    FUNCTION = "perform_join_string_list"
     CATEGORY = "text utility"
 
-    def perform_split_string(self, text, separator):
-        return (text.split(separator), )
+    def perform_join_string_list(self, texts, separator):
+        return (separator[0].join(texts), )
